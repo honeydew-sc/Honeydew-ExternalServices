5fef0d95235ab387e29bcadab9fcdcd045e221e6
Daniel Gempesaw <gempesaw@gmail.com>
2015-04-23 18:44:59 -0400

    Allow daemonize fn to be exported from main module

--- lib/Honeydew/ExternalServices.pm
+++ lib/Honeydew/ExternalServices.pm
@@ -1,8 +1,12 @@
-use strict;
-use warnings;
 package Honeydew::ExternalServices;
 
-# ABSTRACT:
+# ABSTRACT: Helper functions for managing Honeydew's external services
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw/Exporter/;
+our @EXPORT_OK = qw/daemonize/;
 
 =for markdown [![Build Status](https://travis-ci.org/honeydew-sc/Honeydew-ExternalServices.svg?branch=master)](https://travis-ci.org/honeydew-sc/Honeydew-ExternalServices)
 
@@ -11,5 +15,15 @@ package Honeydew::ExternalServices;
 =head1 DESCRIPTION
 
 =cut
+
+sub daemonize {
+    chdir("/")                      || die "can't chdir to /: $!";
+    open(STDIN,  "< /dev/null")     || die "can't read /dev/null: $!";
+    open(STDOUT, "> /dev/null")     || die "can't write to /dev/null: $!";
+    defined(my $pid = fork())       || die "can't fork: $!";
+    exit if $pid;                   # non-zero now means I am the parent
+    (setsid() != -1)                || die "Can't start a new session: $!";
+    open(STDERR, ">&STDOUT")        || die "can't dup stdout: $!";
+}
 
 1;
