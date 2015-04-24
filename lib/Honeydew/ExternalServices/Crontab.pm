9eb318f37655b89e1419b55a4a88e800d9dc457b
Daniel Gempesaw <gempesaw@gmail.com>
2015-04-24 11:14:22 -0400

    Finish first working version of crontab manager

--- lib/Honeydew/ExternalServices/Crontab.pm
+++ lib/Honeydew/ExternalServices/Crontab.pm
@@ -9,57 +9,121 @@ use feature qw/say/;
 use Test::More;
 use Test::Deep;
 
-# say $_ for @{ update_crontab() };
+require Exporter;
+our @ISA = qw/Exporter/;
+our @EXPORT_OK = qw/add_crontab_section remove_crontab_section/;
 
-sub remove_existing_entry {
-    my ($file) = @_;
-    my @current_crontab = @{ _validate_args($file) };
+=head1 SYNOPSIS
+
+In add-to-crontab.pl:
+
+    my $new_crontab = add_crontab_section( 'label', [ '# section' ] );
+    say $_ for @$new_crontab;
+
+Meanwhile,
+
+    # test to see what the output will look like
+    $ add-to-crontab.pl
+
+    # update the crontab
+    $ crontab add-to-crontab.pl
+
+=head1 DESCRIPTION
+
+We need a bunch of different services for Honeydew and Kabocha to
+run. They all want to put things in the crontab. This module enables
+us to make sections in the crontab by using add_crontab_section.
+
+=method add_crontab_section( $label, $section[, $crontab ] )
+
+Takes a String C<$label>, an ArrayRef of a new C<$section>, and
+optionally an ArrayRef of an existing C<$crontab>.
+
+Returns an ArrayRef of a crontab with the section at the end. If the
+section matches a label that was already in the crontab, it will be
+removed, and the new <$section> will be added to the end of the
+resulting ArrayRef.
+
+The label will be used in a comment to denote sections like such:
+
+    ### start: label
+    * * * * * ls -al
+    ### end: label
+
+The section will be inserted between the labels.
+
+This method is idempotent.
+
+=cut
+
+sub add_crontab_section {
+    my ($label, $section, $crontab) = @_;
+    $crontab //= _get_existing_crontab();
+
+    my $filtered_crontab = remove_crontab_section( $label, $crontab );
+    my $labeled_section = _create_crontab_stub( $label, $section );
+    my $appended_crontab = [ @$filtered_crontab, @$labeled_section ];
+
+    return $appended_crontab;
+}
+
+sub _create_crontab_stub {
+    my ($label, $section) = @_;
+
+    my $header = '### start: ' . $label;
+    my $footer = '### end: '   . $label;
+
+    return [ $header, @$section, $footer ];
+}
+
+
+sub _get_existing_crontab {
+    return split("\n", `crontab -l`);
+}
+
+=method remove_existing_section( $label[, $crontab ] )
+
+Takes a String C<$label>, and optionally an ArrayRef of an existing
+C<$crontab>. It will run through the crontab looking for the section
+denoted by the C<$label> and filter it out, returning an ArrayRef of
+the filtered crontab.
+
+If the crontab has manually inserted labels, this method will probably
+screw up.
+
+If no section is found, this is essentially a no-op.
+
+=cut
+
+sub remove_crontab_section {
+    my ($label, $crontab) = @_;
+    $crontab //= _get_existing_crontab();
 
     my @filtered = ();
-    my $in_bmp_section = 0;
-    foreach my $line (@current_crontab) {
-        if ($line =~ /^###.*browsermob$/) {
-            $in_bmp_section = !$in_bmp_section;
+    my $in_section = 0;
+    foreach my $line (@$crontab) {
+        if ($line =~ /^### start: $label/) {
+            $in_section = 1;
+        }
+        elsif ($line =~ /^### end: $label/) {
+            $in_section = 0;
             next;
         }
-        next if $in_bmp_section;
 
-        push @filtered, $line;
+        if ($in_section) {
+            next;
+        }
+        else {
+            push @filtered, $line;
+        }
     }
 
-    push @filtered, '';
+    # Crontabs need an empty line at the end to be valid.
+    if ($filtered[-1] ne '') {
+        push @filtered, '';
+    }
 
     return \@filtered;
-}
-
-sub add_browsermob {
-    my ($file) = @_;
-    my @current = @{ _validate_args($file) };
-
-    push @current, @{ get_bmp_crontab_entry() }, "";
-
-    return \@current;
-}
-
-sub _validate_args {
-    my ($maybe_ref) = @_;
-    my @array;
-
-    if (ref($maybe_ref) eq 'ARRAY') {
-        @array = @$maybe_ref;
-    }
-    else {
-        @array = split("\n", `crontab -l`);
-    }
-
-    return \@array;
-}
-
-sub update_crontab {
-    my $filtered_crontab = remove_existing_entry();
-    my $updated_crontab = add_browsermob($filtered_crontab);
-
-    return $updated_crontab;
 }
 
 1;
--- t/Honeydew-Crontab.t
+++ t/Honeydew-Crontab.t
@@ -2,24 +2,105 @@
 
 use strict;
 use warnings;
-use Test::Spec;
 
-BEGIN: {
-    unless (use_ok('Honeydew::ExternalServices::Crontab')) {
-        BAIL_OUT("Couldn't load Honeydew::ExternalServices::Crontab");
-        exit;
-    }
-}
+use Cwd qw/abs_path/;
+use File::Spec;
+use File::Basename qw/dirname/;
+use Honeydew::ExternalServices::Crontab qw/add_crontab_section remove_crontab_section/;
+use Test::Spec;
+use Test::Deep;
+
 
 describe 'Crontab manager' => sub {
-    my ($hi);
+    my ($crontab, $section);
+    my $fixture_filename = File::Spec->catfile(
+        abs_path( dirname(__FILE__) ),
+        'fixtures/crontab'
+    );
+
+    open (my $fh, '<', $fixture_filename);
+    my @fixture = map { chomp; $_ } <$fh>;
+    close ($fh);
+
     before each => sub {
-        $hi;
+        $crontab = \@fixture;
+        $section = '* * * * * new section';
     };
 
-    it 'should do things' => sub {
-        $hi;
+    it 'should add something new to the crontab' => sub {
+        my $new_crontab = add_crontab_section( 'add me', [ $section ] , $crontab );
+
+        my $expected = [
+            @$crontab,
+            '',
+            '### start: add me',
+            $section,
+            '### end: add me',
+        ];
+
+        cmp_deeply( $new_crontab, $expected);
     };
+
+    it 'should create a crontab stub out of a label and section' => sub {
+        my $stub = Honeydew::ExternalServices::Crontab::_create_crontab_stub( 'label', [ $section ] );
+        cmp_deeply( $stub, [
+            '### start: label',
+            $section,
+            '### end: label'
+        ]);
+    };
+
+    it 'should remove an existing section from a crontab' => sub {
+        my $filtered_crontab = remove_crontab_section( 'remove me', $crontab );
+
+        cmp_deeply( $filtered_crontab, [
+            '',
+            '* * * * * ls -al',
+            '',
+            '# remains',
+            '',
+            '# blank lines',
+            ''
+        ]);
+    };
+
+    it 'should move an existing label to the bottom' => sub {
+        my $replaced_crontab = add_crontab_section( 'remove me', [ $section ], $crontab );
+
+        cmp_deeply( $replaced_crontab, [
+            '',
+            '* * * * * ls -al',
+            '',
+            '# remains',
+            '',
+            '# blank lines',
+            '',
+            '### start: remove me',
+            '* * * * * new section',
+            '### end: remove me',
+        ]);
+    };
+
+    it 'should add sections idempotently' => sub {
+        my $replaced_crontab = add_crontab_section( 'remove me', [ $section ], $crontab );
+        my $idempotent_crontab = add_crontab_section( 'remove me', [ $section ], $replaced_crontab );
+        $idempotent_crontab = add_crontab_section( 'remove me', [ $section ], $idempotent_crontab );
+
+        cmp_deeply( $idempotent_crontab, [
+            '',
+            '* * * * * ls -al',
+            '',
+            '# remains',
+            '',
+            '# blank lines',
+            '',
+            '### start: remove me',
+            '* * * * * new section',
+            '### end: remove me',
+        ]);
+    };
+
+
 
 };
 
